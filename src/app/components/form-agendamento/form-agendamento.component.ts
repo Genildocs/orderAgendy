5dd8f36232315abbfbfc6ba145f7d5f448b1a825
genildocs <genildocs@gmail.com>
2024-02-27 20:37:01 -0300

    commit

--- src/app/components/form-agendamento/form-agendamento.component.ts
+++ src/app/components/form-agendamento/form-agendamento.component.ts
@@ -1,12 +1,13 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-form-agendamento',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './form-agendamento.component.html',
-  styleUrl: './form-agendamento.component.css'
+  styleUrl: './form-agendamento.component.css',
 })
 export class FormAgendamentoComponent {
-
+  loginInput: string = '';
+  constructor() {}
 }
